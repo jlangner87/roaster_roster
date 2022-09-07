2669f40425b5151cac84a4035548a6eb0ee3e290
jlangner87 <josh.langner@gmail.com>
2022-09-06 21:43:49 -0500

    attempt deploy to heroku

--- project_version_dbs/icdb2_django/settings.py
+++ project_version_dbs/icdb2_django/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'icdb2',
-    'rest_framework'
+    'rest_framework',
+    'simple_deploy'
 ]
 
 REST_FRAMEWORK = {
